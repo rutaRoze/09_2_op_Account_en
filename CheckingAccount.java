9b54e79e598d9841bd69ac4b7277d296293548bf
Rūta Rožėnė <rozene.ruta@gmail.com>
2023-11-14 00:32:09 +0200

    Added homework.

---
+++ CheckingAccount.java
@@ -0,0 +1,21 @@
+public class CheckingAccount extends Account {
+
+    private double overdraftLimit;
+
+    public CheckingAccount(int id, double balance, double overdraftLimit) {
+        super(id, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public void withdraw(double amount) {
+        if (super.getBalance() + this.overdraftLimit >= amount) {
+            super.setBalance(super.getBalance() - amount);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CheckingAccount: " + super.getBalance() +
+                ", overdraftLimit=" + overdraftLimit;
+    }
+}
